a91051bab13897b3b18d7ce68ca41aace22da4a3
Neville Brem <bremneville@gmail.com>
2024-02-07 13:28:08 +0100

    style: added styles, feat: tweaked hero

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        bmOrange: "#FF9722",
+        bmPink1: "#FF2291",
+        bmPink2: "#FF6AAF",
+        bmPink3: "#FE94C4",
+        bmPink4: "#FEB4D6",
+        bmBlue1: "#3E56FF",
+        bmBlue2: "#697BFF",
+        bmBlue3: "#939FFF",
+        bmBlue4: "#B8C1FF",
+      },
+    },
+  },
+  plugins: [],
+};
