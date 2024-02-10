439cccf7e776f8e93aacd83ef9d9bf5a532d1d0e
Neville Brem <bremneville@gmail.com>
2024-02-10 16:47:57 +0100

    refactor: got rid of React components

---
+++ src/components/Footer.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import Bar from "../assets/BAR.png";
+
+  const founders: founder[] = [
+    {
+      name: "Neville",
+      link: "nevillebrem",
+    },
+    {
+      name: "Marko",
+      link: "marking27",
+    },
+    {
+      name: "Eljas",
+      link: "elb4s",
+    },
+  ];
+</script>
+
+<footer>
+  <div class="grid md:grid-flow-col grid-flow-row text-secondary p-4 md:pt-10">
+    <div
+      class="text-center md:text-left flex flex-col md:flex-row gap-4 justify-center mb-2 md:mb-0"
+    >
+      <img src={Bar.src} alt="barmates logo" class="h-32 hidden md:block" />
+      <div class="mx-auto md:mx-0 md:my-auto">
+        <h1 class="flex text-4xl md:text-6xl">
+          {#each "BarMates".split("") as letter}
+            <span class="hover:text-primary duration-300 ease-in-out">
+              {letter}
+            </span>
+          {/each}
+        </h1>
+        <p class="font-semibold">Zurich, Switzerland</p>
+      </div>
+    </div>
+    <div class="grid grid-flow-col text-lg justify-center gap-16">
+      <div>
+        <h1>Founders</h1>
+        <ul class="font-semibold text-secondary">
+          {#each founders as founder}
+            <li>
+              <a href={`https://instagram.com/${founder.link}`} target="_blank">
+                {founder.name}
+              </a>
+            </li>
+          {/each}
+        </ul>
+      </div>
+
+      <div>
+        <h1>Social</h1>
+        <ul class="font-semibold text-secondary">
+          <li>
+            <a href="https://instagram.com/barmates_official" target="_blank"
+              >Instagram</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+<div
+  class="text-center py-1 border-t border-secondary font-bold font-[Montserrat] text-secondary"
+>
+  <a href="https://github.com/nevthereal/barmates">© Neville Brem 2023</a>
+</div>
